89a82b7851d6731dc044abd036ee79d94f4364cf
nadeeraka <nimanthanadeeraka@outlook.com>
2019-03-03 14:00:30 +0530

    nav added

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,42 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import Navbar from "./layouts/Navbar"
 import "./layout.css"
+import "./bootstrap.min.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <>
+    <Navbar />
+    {children}
+  </>
 )
 
 Layout.propTypes = {
